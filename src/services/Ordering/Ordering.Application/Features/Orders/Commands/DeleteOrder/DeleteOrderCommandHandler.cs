809958c15842ed3075255dc7f7fc166a16575e55
ravikirang acharya <gravikiranacharya@gmail.com>
2022-01-17 20:27:28 +0530

    Completed Ordering Application

---
+++ src/services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepostory _orderRepostory;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepostory orderRepostory, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepostory = orderRepostory;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepostory.GetByIdAsync(request.Id);
+            if (orderToDelete == null)
+            {
+                _logger.LogError("Order does not exist on DB.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+            await _orderRepostory.DeleteAsync(orderToDelete);
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully updated.");
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepostory _orderRepostory;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepostory orderRepostory, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepostory = orderRepostory;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepostory.GetByIdAsync(request.Id);
+            if (orderToUpdate == null)
+            {
+                _logger.LogError("Order does not exist on DB.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+            await _orderRepostory.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+            return Unit.Value;
+        }
+    }
+}
