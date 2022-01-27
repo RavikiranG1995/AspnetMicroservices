d434cd38afeb28e32c6f872f8b35252eb0ecd070
ravikirang acharya <gravikiranacharya@gmail.com>
2022-01-27 22:30:05 +0530

    Completed implementaion and consumption of MassTransit-RabbitMq

--- src/services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/services/Basket/Basket.API/Controllers/BasketController.cs
@@ -4,6 +4,9 @@ using Basket.API.Repositories;
 using Basket.API.Entities;
 using System.Net;
 using Basket.API.GrpcServices;
+using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
 
 namespace Basket.API.Controllers
 {
@@ -13,11 +16,15 @@ namespace Basket.API.Controllers
     {
         private readonly IBasketRepository _repository;
         private readonly DiscountGrpcService _discountGrpcService;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IPublishEndpoint publishEndpoint, IMapper mapper)
         {
             _repository = repository;
             _discountGrpcService = discountGrpcService;
+            _publishEndpoint = publishEndpoint;
+            _mapper = mapper;
         }
         [HttpGet("{userName}", Name = "GetBasket")]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
@@ -44,6 +51,29 @@ namespace Basket.API.Controllers
         {
             await _repository.DeleteBasket(userName);
             return Ok();
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType((int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckout basketCheckout)
+        {
+            //get existing basket with total price
+            var basket = await _repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+            //create basket checkout event
+            var eventMessage = _mapper.Map<BasketCheckoutEvent>(basketCheckout);
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await _publishEndpoint.Publish(eventMessage);
+
+            //remove from redis cache
+            await _repository.DeleteBasket(basketCheckout.UserName);
+
+            return Accepted();
         }
     }
 }
---
+++ src/services/Basket/Basket.API/Mapper/BasketProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using EventBus.Messages.Events;
+
+namespace Basket.API.Mapper
+{
+    public class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
